053a0f140b4705e33eb9f03506f9f55d8e7f1065
machine <analyticrye@gmail.com>
2019-09-24 14:29:56 -0400

    connected to mongoDB Atlas

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
+//connect database
+connectDB();
 app.get("/", (req, res) => res.send("API Running"));
 
 const PORT = process.env.PORT || 3000;
