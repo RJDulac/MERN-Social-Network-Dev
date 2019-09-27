6a6fe14e4fed55c0caed1a7cd89b31a800744bd8
machine <analyticrye@gmail.com>
2019-09-27 03:51:07 -0400

    create and delete education added - bug where id doesn't match. Maybe try checking if array contains the id being searched(req.params.edu_id). same bug for experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -218,12 +218,102 @@ router.put(
 router.delete("/experience/:exp_id", auth, async (req, res) => {
   try {
     const profile = await Profile.findOne({ user: req.user.id });
+
+    //need better solution than checking the lenght of the array
+    if (profile.experience.length < 1) {
+      return res.status(400).json({ msg: "Invalid ID" });
+    }
+
     //get remove index
     const removeIndex = profile.experience
       .map(item => item.id)
       .indexOf(req.params.exp_id);
 
     profile.experience.splice(removeIndex, 1);
+
+    await profile.save();
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+//update profile with education - private
+router.put(
+  "/education",
+  [
+    auth,
+    [
+      check("school", "School is required")
+        .not()
+        .isEmpty(),
+      check("degree", "Degree is required")
+        .not()
+        .isEmpty(),
+      check("fieldofstudy", "Field of study is required")
+        .not()
+        .isEmpty(),
+      check("from", "From date is required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description
+    } = req.body;
+    const newEdu = {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description
+    };
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+      if (!profile) {
+        res.status(400).json({ msg: "User Not Found" });
+      }
+      profile.education.unshift(newEdu);
+      await profile.save();
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+//delete education from profile - private
+router.delete("/education/:edu_id", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+    //get remove index
+    //bug removes with invalid id. Need check for id
+    //need better solution than checking the lenght of the array
+    if (profile.education.length < 1) {
+      return res.status(400).json({ msg: "Invalid ID" });
+    }
+
+    const removeIndex = profile.education
+      .map(item => item.id)
+      .indexOf(req.params.edu_id);
+
+    profile.education.splice(removeIndex, 1);
 
     await profile.save();
 
